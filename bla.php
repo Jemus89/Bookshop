3441ddff2254f12004522c0dc530b284ee24e9b6
Peter Unterthurner <Peter Unterthurner>
2017-04-11 16:36:58 +0200

    Initial commit

---
+++ bla.php
@@ -0,0 +1,4 @@
+<?php
+$neu = htmlspecialchars("<a href='test'>Test</a>", ENT_QUOTES);
+echo $neu; // &lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;
+?>
---
+++ le book.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>le book</title>
+    <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+</head>
+<body>
+<div style="height: 20px">
+
+</div>
+<div class="div-loginMenu">
+    <div class="col-md-6 div-topmenubar">
+        <div class="div-icon">
+            <div class="div-le" style="">
+                L E
+            </div>
+            <div>
+                B O O K
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 div-topmenubar">
+        <div class="div-search">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search for...">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">
+                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                    </button>
+                </span>
+            </div>
+        </div>
+        <div class="div-basket">
+            <button class="button-shopping" onclick="alert('hi')">
+                <img src="icon/cart.png">
+            </button>
+        </div>
+        <div class="div-login">
+            <a>login</a>
+        </div>
+    </div>
+</div>
+<div class="div-topmenu">
+    <div class="col-md-6 div-topmenubar">
+        <div class="div-menucontainer">
+            <button class="button-TopMenu" onclick="alert('hi')">
+                <img src="icon/crime.png">
+                <p class="p-topMenu">Crime</p>
+            </button>
+        </div>
+        <div class="div-menucontainer">
+            <button class="button-TopMenu">
+                <img src="icon/thriller.png">
+                <p class="p-topMenu">Thriller</p>
+            </button>
+        </div>
+        <div class="div-menucontainer">
+            <button class="button-TopMenu">
+                <img src="icon/romance.png">
+                <p class="p-topMenu">Romance</p>
+            </button>
+        </div>
+        <div class="div-menucontainer">
+            <button class="button-TopMenu">
+                <img src="icon/scifi.png">
+                <p class="p-topMenu">Sci-Fi</p>
+            </button>
+        </div>
+    </div>
+    <div class="col-md-6 div-topmenubar">
+        <div class="div-menucontainer">
+            <button class="button-TopMenu">
+                <img src="icon/fantasy.png">
+                <p class="p-topMenu">Fantasy</p>
+            </button>
+        </div>
+        <div class="div-menucontainer">
+            <button class="button-TopMenu">
+                <img src="icon/horror.png">
+                <p class="p-topMenu">Horror</p>
+            </button>
+        </div>
+        <div class="div-menucontainer">
+            <button class="button-TopMenu">
+                <img src="icon/historic.png">
+                <p class="p-topMenu">Historic</p>
+            </button>
+        </div>
+        <div class="div-menucontainer">
+            <button class="button-TopMenu">
+                <img src="icon/comics.png">
+                <p class="p-topMenu">Comics</p>
+            </button>
+        </div>
+    </div>
+</div>
+</body>
+</html>
