92d18b98bcceacaa1ffe72f4dc2845ce6f1dfb40
Jemus89 <dreamangel-italy@hotmail.com>
2017-04-12 11:05:18 +0200

    just a testeroni

--- le book.php
+++ le book.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>le book</title>
+    <title>Le Book</title>
     <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
